b44a72aec306e696da1e536fec64907c9b84bf9a
gerizzo <gerardorizzo99@gmail.com>
2022-05-03 03:23:33 -0300

    MEJORA ENTREGA DETALLE DE PRODUCTO

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <Navbar2></Navbar2>
       <main>
         <ItemListContainer></ItemListContainer>
+        <ItemListContainer></ItemListContainer>
       </main>
     </div>
   );
